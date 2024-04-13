7179c6b5d20dcfea22c8eca5e1dabe4d031c24d9
robsontureck <163215042+robsontureck@users.noreply.github.com>
2024-04-13 22:32:13 +1000

    Commit of the project.

---
+++ Athlete.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using static System.Console;
+
+namespace QUTChampionship
+{
+    class Athlete
+    {
+        //Fields
+        private char code;
+        private string description;
+        public static char[] codes = { 'T', 'B', 'S', 'R', 'G'};
+        public static string[] categories = { "Tennis", "Badminton", "Swimming", "Running", "Gymnastics"}; 
+        private char invalid = 'I';
+
+        //Auto-implemented property
+        public string name { get; set; }
+
+        //Properties
+
+        public char Code
+        {
+            get { return code; }
+            set
+            {
+                int pos = -1;
+                for (int c = 0; c < codes.Length; ++c)
+                {
+                    if (value == codes[c])  // checks if code is valid
+                    {
+                        code = value;
+                        pos = c;
+                        AssignDescription();
+                    }
+                }
+                if (pos < 0)
+                {
+                    code = invalid;
+                    AssignInvalidDescription();
+                }
+                
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+        }
+
+        public void AssignDescription()  // If code is valid assign a description from description array
+        {
+            for(int i =0; i<categories.Length; ++i)
+            {
+                if(code == codes[i])
+                {
+                    description = categories[i];
+                }
+            }
+        }
+
+        public void AssignInvalidDescription()
+        {
+            description = "Invalid entry";
+        }
+
+        //Methods
+
+        public static void DisplayCategories()
+        {
+            WriteLine("\n-----------------------");
+            WriteLine(" Valid Event Categories");
+            WriteLine("-----------------------");
+            WriteLine("Event Category | Code");
+            WriteLine("-----------------------");
+            for (int i = 0; i< codes.Length; ++i)
+            {   
+                WriteLine("{0,-10}     | {1,2}", categories[i], codes[i]);
+                WriteLine("-----------------------");
+            }
+        }
+  
+    }
+}
---
+++ QutChampionshipApp.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace QUTChampionship
+{
+    class QutChampionshipApp
+    {
+        static void Main()
+        {
+            DisplayStudentNumber();
+            // a.
+            NumberOfParticipants(out int participants);
+            
+            // b/
+            DisplayRevenue(participants); 
+           
+            //Create instances
+            Athlete[] athletes = new Athlete[participants];  // Create array of athletes
+
+            // c.
+            for(int i = 0; i<athletes.Length; ++i)
+            {
+                athletes[i] = new Athlete();
+                athletes[i].name = AskForAthleteName(i);
+                athletes[i].Code = AskForAthleteCode(i);
+            }
+            
+            //Calling methods
+            // d.
+            DisplayAllAthletes(athletes);  // Display information of all participants
+
+            // e.
+            DisplayAthletesinCategory(athletes); // Display participants in category entered by user
+
+        }
+
+        //Methods
+
+        static void DisplayStudentNumber()
+        {
+            WriteLine("Program create by N11416289\n");
+        }
+
+        static void NumberOfParticipants(out int participants)  // Ask user for number of participants
+        {
+            Write("Please enter the number of participants in this year's championship: ");
+            while (!(int.TryParse(ReadLine(), out participants)) || participants < 0 || participants > 30)  //Ask user for number of athletes and only accepts valid numbers
+            {
+                Write("Please enter a valid number between 0 and 30: ");
+            }
+
+        }
+        static void DisplayRevenue(int participants)  // Calculate and display revenue
+        {
+            const int cost = 20;
+            WriteLine("\n------------------------------------------------------------------");
+            WriteLine("The championship have {0} participants and the revenue is {1}", participants, (participants * cost).ToString("C"));
+            WriteLine("------------------------------------------------------------------");
+        }
+
+        static string AskForAthleteName(int i)  // Prompt user for athlete name
+        {
+            Write("\nPlease enter athlete {0} name: ", i+1);
+            string name = ReadLine();
+            return name;
+        }
+
+        static char AskForAthleteCode(int i)  // Prompt user for athlete code and display valid codes 
+        {
+            char code;
+            Athlete.DisplayCategories();
+            Write("\nPlease enter athlete's {0} code: ", i+1);
+            while (!(char.TryParse(ReadLine(), out code)))
+            {
+                Write("\nPlease enter a valid code for athlete {0}: ", i+1);
+            }
+            return code;
+
+        }
+
+        static void DisplayAllAthletes(Athlete[] athletes)  // Display all participants information
+        {
+            WriteLine("\n--------------------------------");
+            WriteLine("     All Participants");
+            WriteLine("--------------------------------");
+            WriteLine("{0,-10} | {1,-5} | {2,5}", "Name", "Code", "Description");
+            for (int i = 0; i < athletes.Length; ++i)
+            {
+                WriteLine("--------------------------------");
+                WriteLine("{0,-10} |  {1,-5}|  {2,5} ", athletes[i].name, athletes[i].Code, athletes[i].Description);
+            }
+        }
+
+        static void DisplayAthletesinCategory(Athlete[] athletes)  // Display participants in category entered by user
+        {
+            char STOP = '!';
+            int pos = -1;
+            char pCode;
+            Athlete.DisplayCategories(); // Display valid categories and events 
+            Write("Please enter an event code or '!' to exit: "); 
+            while (!(char.TryParse(ReadLine(), out pCode)))
+            {
+                WriteLine("\nInvalid code, please try again!");
+                Write("\nPlease enter an event code or '!' to exit: ");
+            }
+            while (pCode != STOP)
+            {
+                for(int c = 0; c < Athlete.codes.Length; ++c)
+                {
+                    if(pCode == Athlete.codes[c])
+                    {
+                        pos = c;
+                    }
+                }
+                if(pos>=0)
+                {
+                    WriteLine("\n--------------------------------");
+                    WriteLine("    Athletes in {0} category", Athlete.categories[pos]);
+                    WriteLine("--------------------------------");
+                    WriteLine("{0,-10} | {1,-5} | {2,5}", "Name", "Code", "Description");
+                    for (int i = 0; i < athletes.Length; ++i)
+                        if(athletes[i].Code == pCode)
+                        {
+                       
+                            WriteLine("--------------------------------");
+                            WriteLine("{0,-10} |  {1,-5}|  {2,5} ", athletes[i].name, athletes[i].Code, athletes[i].Description);
+                        }
+                }
+                else if(pos<0)
+                {
+                    WriteLine("\nInvalid code, please try again!");
+                }
+                pos = -1;
+                Write("\nPlease enter an event code or '!' to exit: ");
+                while (!(char.TryParse(ReadLine(), out pCode)))
+                {
+                    WriteLine("\nInvalid code, please try again!");
+                    Write("\nPlease enter an event code or '!' to exit: ");
+                }
+
+            }
+            
+
+        }
+
+
+    }
+}
